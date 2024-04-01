1bbc1f3f4d77a10b3bed671719ea7d797823f804
Danya-sketh <yannitc7@gmail.com>
2024-04-01 17:39:25 +0300

    фф

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,28 @@
-const API_KEY = '43113901-6bef41cb21468ce176a9fbb77'; // Встановіть ваш унікальний ключ доступу до API тут
 
-export async function fetchImages(searchQuery) {
-    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Failed to fetch images');
-        }
-        const data = await response.json();
-        return data.hits;
-    } catch (error) {
-        console.error('Error fetching images:', error);
-        throw error;
-    }
-}
+
+
+
+
+
+import axios from "axios";
+
+export async function getImage(query, currentPage) {
+  const BASE_URL = "https://pixabay.com/api/"
+  const params = {
+    key: "43113901-6bef41cb21468ce176a9fbb77",
+    q: query,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+    per_page: 15,
+    page: currentPage,
+  };
+
+  const res = await axios.get(BASE_URL, { params });
+  return res.data;
+};
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,22 +1,134 @@
 
-searchForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const searchTerm = searchInput.value.trim();
-    if (!searchTerm) {
-        showMessage('Please enter a search term');
-        return;
-    }
-    loader.classList.add('show'); // Показуємо індикатор завантаження
+
+import { getImage } from "./js/pixabay-api"
+import { renderImages } from "./js/render-functions";
+import { card } from "./js/render-functions";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+export const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+export const preloader = document.querySelector(".loader");
+const form = document.querySelector(".search-form");
+const buttonLoadMore = document.querySelector(".btn-load-more")
+
+function showLoader() {
+  preloader.classList.remove("is-hidden")
+};
+export function hideLoader() {
+  preloader.classList.add("is-hidden");
+};
+
+function showLoadMore() { 
+  buttonLoadMore.classList.remove("is-hidden");
+};
+function hideLoadMore() {
+  buttonLoadMore.classList.add("is-hidden");
+};
+
+let inputValue;
+let currentPage = 1;
+let maxPage = 0;
+const perPage = 15;
+
+form.addEventListener("submit", sendForm);
+
+async function sendForm(event) {
+  event.preventDefault();
+  hideLoadMore();
+  showLoader();
+  card.innerHTML = "";
+  currentPage = 1;
+  inputValue = event.target.elements.search.value.trim();
+  if (!inputValue) {
+    iziToast.show({
+      message: 'Please complete the field!',
+      theme: 'dark',
+      progressBarColor: '#FFFFFF',
+      color: '#EF4040',
+      position: 'topRight',
+    });
+  };
     try {
-        const images = await fetchImages(searchTerm);
-        if (images.length === 0) {
-            showMessage('Sorry, there are no images matching your search query. Please try again!');
-        } else {
-            renderImages(images);
-        }
+    const data = await getImage(inputValue, currentPage);
+      maxPage = Math.ceil(data.totalHits / perPage)
+      if (arr.length === 0) {
+      iziToast.error({
+      message: 'Sorry, there are no images matching your search query. Please try again!',
+      theme: 'dark',
+      progressBarColor: '#FFFFFF',
+      color: '#EF4040',
+      position: 'topRight',
+    });
+      } else { renderImages(data.hits) };
+      // renderImages(data.hits);
+      // checkButtonStatus();
     } catch (error) {
-        showMessage('Failed to fetch images. Please try again later.');
-    } finally {
-        loader.classList.remove('show'); // Ховаємо індикатор завантаження навіть у випадку помилки
-    }
-});
+      iziToast.error({
+      message: 'Sorry, an error occurred while loading. Please try again!',
+      theme: 'dark',
+      progressBarColor: '#FFFFFF',
+      color: '#EF4040',
+      position: 'topRight',
+    })
+  };
+  // else {
+    // iziToast.show({
+    //   message: 'Please complete the field!',
+    //   theme: 'dark',
+    //   progressBarColor: '#FFFFFF',
+    //   color: '#EF4040',
+    //   position: 'topRight',
+    // });
+  // };
+  hideLoader();
+  form.reset();
+};
+
+buttonLoadMore.addEventListener("click", onLoadMore);
+
+async function onLoadMore() {
+  currentPage += 1;
+  hideLoadMore();
+  showLoader();
+  try {const data = await getImage(inputValue, currentPage);
+    renderImages(data.hits);
+  } catch (error) {
+      iziToast.error({
+      message: 'Sorry, an error occurred while loading. Please try again!',
+      theme: 'dark',
+      progressBarColor: '#FFFFFF',
+      color: '#EF4040',
+      position: 'topRight',
+    })
+  }
+  hideLoader();
+  myScroll();
+  checkButtonStatus();
+};
+
+function checkButtonStatus() {
+  if (currentPage >= maxPage) {
+    hideLoadMore();
+      iziToast.show({
+      message: "We're sorry, but you've reached the end of search results.",
+      theme: "dark",
+      progressBarColor: "#FFFFFF",
+      color: "blue",
+      position: "topRight",
+    });
+  } else {
+  showLoadMore();
+  }
+};
+
+function myScroll() {
+  const height = card.firstChild.getBoundingClientRect().height;
+
+  scrollBy({
+    top: height,
+    behavior: 'smooth',
+  });
+}
+
